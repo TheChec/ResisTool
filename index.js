9aef56e1707f5db5c2fb7ece68c5b9cf2ff10795
TheChec <chechoosorio@hotmail.com>
2024-05-24 18:03:28 -0500

    Backend

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const divinputsSerie = document.getElementById('inputsSerie');
 add1.addEventListener('click', e => {
     let div = document.createElement('div')
     div.innerHTML = `<label>R2</label>
-                    <input type="number" name="" id="" autocomplete="off" required> <button class="btnDelete" id="deleteButton" onclick='deletes0(this)'> X </button>`
+                    <input value="0" type="number" placeholder="Datos en Ω" id="ResistenceSerie" autocomplete="off" required> <button class="btnDelete" id="deleteButton" onclick='deletes0(this)'> X </button>`
     divinputsSerie.appendChild(div)
     updateTotal();
 })
@@ -13,7 +13,7 @@ const divinputsParalele = document.getElementById('inputsParalele');
 add2.addEventListener('click', e => {
     let div = document.createElement('div')
     div.innerHTML = `<label>R2</label>
-                    <input type="number" name="" id="" autocomplete="off" required> <button class="btnDelete" id="deleteButton" onclick='deletes1(this)'> X </button>`
+                    <input value="0" type="number" placeholder="Datos en Ω" id="ResistenceParalale" autocomplete="off" required> <button class="btnDelete" id="deleteButton" onclick='deletes1(this)'> X </button>`
     divinputsParalele.appendChild(div)
     updateTotal();
 })
@@ -23,7 +23,7 @@ const divinputsSerieCap = document.getElementById('inputsSerieCap');
 add3.addEventListener('click', e => {
     let div = document.createElement('div')
     div.innerHTML = `<label>C2</label>
-                    <input type="number" name="" id="" autocomplete="off" required> <button class="btnDelete" id="deleteButton" onclick='deletes2(this)'> X </button>`
+                    <input value="0" type="number" placeholder="Datos en μF" id="CapacitorSerie" autocomplete="off" required> <button class="btnDelete" id="deleteButton" onclick='deletes2(this)'> X </button>`
     divinputsSerieCap.appendChild(div)
     updateTotal();
 })
@@ -32,7 +32,7 @@ const divinputsParaleleCap = document.getElementById('inputsParaleleCap');
 add4.addEventListener('click', e => {
     let div = document.createElement('div')
     div.innerHTML = `<label>C2</label>
-                    <input type="number" name="" id="" autocomplete="off" required> <button class="btnDelete" id="deleteButton" onclick='deletes3(this)'> X </button>`
+                    <input value="0" type="number" placeholder="Datos en μF" id="CapacitorParalele" autocomplete="off" required> <button class="btnDelete" id="deleteButton" onclick='deletes3(this)'> X </button>`
     divinputsParaleleCap.appendChild(div)
     updateTotal();
 })
@@ -42,7 +42,7 @@ add4.addEventListener('click', e => {
 const deletes0 = (e) => {
     const divFather = e.parentNode;
     divinputsSerie.removeChild(divFather);
-    
+
     updateTotal();
 }
 
@@ -54,7 +54,7 @@ const deletes1 = (e) => {
 const deletes2 = (e) => {
     const divFather = e.parentNode;
     divinputsSerieCap.removeChild(divFather);
-    
+
     updateTotal();
 }
 
@@ -66,33 +66,33 @@ const deletes3 = (e) => {
 
 /*------metodos para actualizar número de inputs----*/
 const updateTotal = () => {
-    let divsSerie = divinputsSerie.children ;
+    let divsSerie = divinputsSerie.children;
     let divsParalele = divinputsParalele.children;
-    let divsSerieCap = divinputsSerieCap.children ;
+    let divsSerieCap = divinputsSerieCap.children;
     let divsParaleleCap = divinputsParaleleCap.children;
     total0 = 1;
     total1 = 1;
     total2 = 1;
     total3 = 1;
-    if(divsSerie.length != 0){
-    for(let i = 0; i < divsSerie.length; i++){
-        divsSerie[i].children[0].innerHTML = "R"+total0++;
-    }
-    if(divsParalele.length != 0){
-        for(let i = 0; i < divsParalele.length; i++){
-            divsParalele[i].children[0].innerHTML = "R"+total1++;
+    if (divsSerie.length != 0) {
+        for (let i = 0; i < divsSerie.length; i++) {
+            divsSerie[i].children[0].innerHTML = "R" + total0++;
         }
-    }
-    if(divsSerieCap.length != 0){
-        for(let i = 0; i < divsSerieCap.length; i++){
-            divsSerieCap[i].children[0].innerHTML = "C"+total2++;
+        if (divsParalele.length != 0) {
+            for (let i = 0; i < divsParalele.length; i++) {
+                divsParalele[i].children[0].innerHTML = "R" + total1++;
+            }
         }
-    }
-    if(divsParaleleCap.length != 0){
-        for(let i = 0; i < divsParaleleCap.length; i++){
-            divsParaleleCap[i].children[0].innerHTML = "C"+total3++;
+        if (divsSerieCap.length != 0) {
+            for (let i = 0; i < divsSerieCap.length; i++) {
+                divsSerieCap[i].children[0].innerHTML = "C" + total2++;
+            }
         }
-    }
+        if (divsParaleleCap.length != 0) {
+            for (let i = 0; i < divsParaleleCap.length; i++) {
+                divsParaleleCap[i].children[0].innerHTML = "C" + total3++;
+            }
+        }
     }
 }
 
@@ -125,11 +125,11 @@ const sliderParaleleRest = document.getElementById('sliderParalele')
 
 btnSerieRest.addEventListener('click', (e) => {
     sliderParaleleRest.style.display = 'none'
-    sliderSerieRest.style.display ='flex';
+    sliderSerieRest.style.display = 'flex';
     sliderSerieRest.style.animation = 'animate 0.5s ease';
 })
 btnParaleleRest.addEventListener('click', (e) => {
-    sliderSerieRest.style.display ='none';
+    sliderSerieRest.style.display = 'none';
     sliderParaleleRest.style.display = 'flex'
     sliderParaleleRest.style.animation = 'animate 0.5s ease';
 })
@@ -153,3 +153,144 @@ btnParaleleCap.addEventListener('click', (e) => {
 
 
 
+/**
+ * ! Metodos relacionados al calculo de las resistencias
+ */
+
+function calcFrac(inputs) {
+    let total = 0
+    console.log(inputs)
+    inputs.forEach(input => {
+        console.log(input.value)
+        let data = parseFloat(input.value)
+        total += 1 / data
+    });
+    total = 1 / total
+    return total
+}
+function calcSumaSimple(inputs) {
+    let total = 0
+    inputs.forEach(input => {
+        console.log(input.value)
+        let data = parseFloat(input.value)
+        total += data
+
+    });
+    return total
+}
+
+const btnCalResistenceSerie = document.getElementById('btnResistenceSerie');
+var adviceAlert = document.getElementById('divAlert');
+var resultResistenceSerie = document.getElementById('resultResistenceSerie')
+btnCalResistenceSerie.addEventListener('click', () => {
+    let inputs = document.querySelectorAll('#ResistenceSerie');
+    let validInputs = Array.from(inputs).every(input => parseFloat(input.value) != 0);
+
+    if (validInputs) {
+        let total = calcSumaSimple(inputs)
+
+        if (total >= 1000) {
+            total = (total / 1000).toFixed(2) + " kΩ";
+        } else {
+            total = total.toFixed(2) + " Ω";
+        }
+
+
+        console.log(total)
+        resultResistenceSerie.innerHTML = total
+    } else {
+        adviceAlert.style.display = 'flex';
+        adviceAlert.style.animation = 'alert 1.5s ease';
+        setTimeout(function () {
+            adviceAlert.style.display = 'none'
+        }, 1400)
+    }
+})
+
+const btnCalResistenceParalele = document.getElementById('btnResistenceParalele');
+var adviceAlert = document.getElementById('divAlert');
+var resultResistenceParalele = document.getElementById('resultResistenceParalele')
+btnCalResistenceParalele.addEventListener('click', () => {
+    let inputs = document.querySelectorAll('#ResistenceParalele');
+    let validInputs = Array.from(inputs).every(input => parseFloat(input.value) != 0);
+
+    if (validInputs) {
+        let total = calcFrac(inputs)
+
+        if (total >= 1000) {
+            total = (total / 1000).toFixed(2) + " kΩ";
+        } else {
+            total = total.toFixed(2) + " Ω";
+        }
+
+
+        console.log(total)
+        resultResistenceParalele.innerHTML = total
+    } else {
+        adviceAlert.style.display = 'flex';
+        adviceAlert.style.animation = 'alert 1.5s ease';
+        setTimeout(function () {
+            adviceAlert.style.display = 'none'
+        }, 1400)
+    }
+})
+
+const btnCalCapacitorSerie= document.getElementById('btnCapacitorSerie');
+var adviceAlert = document.getElementById('divAlert');
+var resultCapacitorSerie = document.getElementById('resultCapacitorSerie')
+btnCalCapacitorSerie.addEventListener('click', () => {
+    let inputs = document.querySelectorAll('#CapacitorSerie');
+    let validInputs = Array.from(inputs).every(input => parseFloat(input.value) != 0);
+
+    if (validInputs) {
+        let total = calcFrac(inputs)
+
+        if (total >= 1e6) {
+            total = (total / 1e6).toFixed(2) + " F";
+        } else if (total >= 1e3) {
+            total = (total / 1e3).toFixed(2) + " mF";
+        } else {
+            total = total.toFixed(2) + " μF";
+        }
+
+
+        console.log(total)
+        resultCapacitorSerie.innerHTML = total
+    } else {
+        adviceAlert.style.display = 'flex';
+        adviceAlert.style.animation = 'alert 1.5s ease';
+        setTimeout(function () {
+            adviceAlert.style.display = 'none'
+        }, 1400)
+    }
+})
+
+const btnCapacitorParalele= document.getElementById('btnCapacitorParalele');
+var adviceAlert = document.getElementById('divAlert');
+var resultCapacitorParalele = document.getElementById('resultCapacitorParalele')
+btnCapacitorParalele.addEventListener('click', () => {
+    let inputs = document.querySelectorAll('#CapacitorParalele');
+    let validInputs = Array.from(inputs).every(input => parseFloat(input.value) != 0);
+
+    if (validInputs) {
+        let total = calcSumaSimple(inputs)
+
+        if (total >= 1e6) {
+            total = (total / 1e6).toFixed(2) + " F";
+        } else if (total >= 1e3) {
+            total = (total / 1e3).toFixed(2) + " mF";
+        } else {
+            total = total.toFixed(2) + " μF";
+        }
+
+
+        console.log(total)
+        resultCapacitorParalele.innerHTML = total
+    } else {
+        adviceAlert.style.display = 'flex';
+        adviceAlert.style.animation = 'alert 1.5s ease';
+        setTimeout(function () {
+            adviceAlert.style.display = 'none'
+        }, 1400)
+    }
+})
